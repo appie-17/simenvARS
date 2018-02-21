375d859811d143edf5ce1697cb6e0ea4765cfc23
Jordy <jordyvanappeven@gmail.com>
2018-02-21 23:29:26 +0100

    Add evolutionary algorithm

--- Tutorial1/Jordy.py
+++ Tutorial1/Jordy.py
@@ -42,7 +42,7 @@ def plotSurface(rn_range, benchmark_function):
     
     Z = Z.reshape(dim)
     # Plot the surface.
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, alpha=0.9)
+    surf = ax.plot_surface(X, Y, Z, linewidths=0, cmap='Greys', alpha=0.9)
     fig.colorbar(surf, shrink=0.5, aspect=5)
     # Customize the z axis.
     # ax.zaxis.set_major_locator(LinearLocator(10))
@@ -53,7 +53,7 @@ def plotSurface(rn_range, benchmark_function):
 
 def gradientDescent(num_iter, rn_range, benchmark_function, benchmark_function_grad):
     # Initialise X
-    X = np.array([[rn()*rn_range[0]+rn_range[1]], 
+    X = np.array([[np.random.rand()*rn_range[0]+rn_range[1]], 
         [np.random.rand()*rn_range[0]+rn_range[1]]])
     # X = np.array([[rn_range[1]], [rn_range[1]]])
     # Gradient descent
@@ -66,23 +66,20 @@ def gradientDescent(num_iter, rn_range, benchmark_function, benchmark_function_g
 def particleSwarmOptimisation(num_iter, num_particles,rn_range, benchmark_function):
     # PSO initialisation particle_best (Sp_best), group_best(Sg_best), 
     #   particle(Sp), particle_velocity(Vp)
-    Sp = np.zeros((num_particles,2))
-    Sp_best = np.zeros((num_particles,2))
+    # Sp = np.zeros((num_particles,2))
+    Sp = np.random.rand(num_particles,2)*rn_range[0]+rn_range[1]
+    Sp_best = Sp
     Sg_best = np.zeros(2).reshape(1,2)
     Vp = np.zeros((num_particles,2))
     a, b, c = 0.3, 0.3, 0.3
 
     output = []
     for i in range(num_particles):
-        Sp[i] = np.array([rn()*rn_range[0]+rn_range[1], 
-            rn()*rn_range[0]+rn_range[1]])
-        # Sp[i] = np.array([rn_range[1], rn_range[1]])
-        Sp_best[i] = Sp[i]
 
         if output == []:
             output = np.array([[Sp[i][0]], [Sp[i][1]], [benchmark_function(Sp[i].reshape(2,1))]])
             Sg_best = Sp[i]
-            
+
         else:
             output = np.hstack((output, np.array([[Sp[i][0]], [Sp[i][1]], [benchmark_function(Sp[i].reshape(2,1))]])))
             if benchmark_function(Sp[i].reshape(2,1)) <= benchmark_function(Sg_best.reshape(2,1)):
@@ -93,7 +90,7 @@ def particleSwarmOptimisation(num_iter, num_particles,rn_range, benchmark_functi
         
         for i in range(num_particles):
             
-            Vp[i] = a * Vp[i] + b*rn()*Sp_best[i] + c*rn()*Sg_best
+            Vp[i] = a * Vp[i] + b*np.random.rand()*Sp_best[i] + c*np.random.rand()*Sg_best
             Sp[i] = Sp[i] * Vp[i]
             if benchmark_function(Sp[i].reshape(2,1)) <= benchmark_function(Sp_best[i].reshape(2,1)):
                 Sp_best[i] = Sp[i]
@@ -104,39 +101,73 @@ def particleSwarmOptimisation(num_iter, num_particles,rn_range, benchmark_functi
             output = np.hstack((output,[[Sp[i][0]],[Sp[i][1]],[benchmark_function(Sp[i].reshape(2,1))]]))
     return output
 
+def evolutionaryAlgorithm(num_iter, initial_population, rn_range, benchmark_function, offspring):
+    #Initialise
+    population = np.random.rand(2,initial_population)*rn_range[0]+rn_range[1]
+    x1, x2 = np.mean(population[0]), np.mean(population[1])
+    output = np.array([[x1],[x2],[benchmark_function(np.array([x1,x2]).reshape(2,1))]])
+    
+    for _ in range(num_iter):    
+        #Evaluation
+        population = np.vstack((population,benchmark_function(population)))
+        #Selection
+        population = population[:,population[2,:].argsort()]
+        population = np.delete(population,2,0)
+        population = population[:,0:round(population.shape[1]*offspring)]
+        #Reproduction
+        population = population.repeat(round(1/offspring),axis=1)
+        #Crossover/Mutation
+        population[0] = [np.mean([population[0,i],population[0,round(np.random.rand()*population.shape[1])-1]]) for i in range(population.shape[1])]
+        population[1] = [np.mean([population[1,i],population[1,round(np.random.rand()*population.shape[1])-1]]) for i in range(population.shape[1])]
+        #Output
+        x1, x2 = np.mean(population[0]), np.mean(population[1])
+        output = np.hstack((output, [[x1],[x2],[benchmark_function(np.array([x1,x2]).reshape(2,1))]]))
+    return output
+
 #Parameters
 num_iter = 250
 step_size = 0.001
 num_particles = 5
+initial_population = 1000
+offspring = 0.2
 #random number range [0] for multiplication and [1] for substraction
-rn = np.random.random
 rn_range = [10,-5]
+scatter_size = 50
+
 
 #Benchmark on rosenbrock
-ax = plotSurface(rn_range, rosenbrock)
+# ax = plotSurface(rn_range, rosenbrock)
 
-GD_plot = gradientDescent(num_iter, rn_range, rosenbrock, rosenbrock_grad)
-ax.scatter(GD_plot[0], GD_plot[1], GD_plot[2], c='b', marker='x')
+# GD_plot = gradientDescent(num_iter, rn_range, rosenbrock, rosenbrock_grad)
+# ax.scatter(GD_plot[0], GD_plot[1], GD_plot[2], c='b', marker='x', s=scatter_size)
 
-PSO_plot = particleSwarmOptimisation(num_iter, num_particles, rn_range, rosenbrock)
-ax.scatter(PSO_plot[0], PSO_plot[1], PSO_plot[2], c='r', marker='x')
-plt.show()
-fmt = '{:<12}{:<25}{}'
-print(fmt.format("Iteration", "Gradient descent ", "Particle Swarm Optimisation"))
-[print(fmt.format(i, gd, pso)) for i, (gd, pso) in enumerate(zip(GD_plot[2], PSO_plot[2]))]
+# PSO_plot = particleSwarmOptimisation(num_iter, num_particles, rn_range, rosenbrock)
+# ax.scatter(PSO_plot[0], PSO_plot[1], PSO_plot[2], c='r', marker='x', s=scatter_size)
+
+# EA_plot = evolutionaryAlgorithm(num_iter, initial_population, rn_range, rosenbrock, offspring)
+# ax.scatter(EA_plot[0], EA_plot[1], EA_plot[2], c='g', marker='x', s=scatter_size)
+
+# plt.show()
+
+# fmt = '{:<12}{:<25}{:<35}{}'
+# print(fmt.format("Iteration", "Gradient descent ", "Particle Swarm Optimisation", "Evolutionary Algorithm"))
+# [print(fmt.format(i, gd, pso, ea)) for i, (gd, pso, ea) in enumerate(zip(GD_plot[2], PSO_plot[2], EA_plot[2]))]
 
 
 #Benchmark on rastrigin
 ax = plotSurface(rn_range, rastrigin)
 
 GD_plot = gradientDescent(num_iter, rn_range, rastrigin, rastrigin_grad)
-ax.scatter(GD_plot[0], GD_plot[1], GD_plot[2], c='b', marker='x')
+ax.scatter(GD_plot[0], GD_plot[1], GD_plot[2], c='b', marker='x', s=scatter_size)
 
 PSO_plot = particleSwarmOptimisation(num_iter, num_particles, rn_range, rastrigin)
-ax.scatter(PSO_plot[0], PSO_plot[1], PSO_plot[2], c='r', marker='x')
+ax.scatter(PSO_plot[0], PSO_plot[1], PSO_plot[2], c='r', marker='x', s=scatter_size)
+
+EA_plot = evolutionaryAlgorithm(num_iter, initial_population, rn_range, rastrigin, offspring)
+ax.scatter(EA_plot[0], EA_plot[1], EA_plot[2], c='g', marker='x', s=scatter_size)
 
 plt.show()
 
-fmt = '{:<12}{:<25}{}'
-print(fmt.format("Iteration", "Gradient descent ", "Particle Swarm Optimisation"))
-[print(fmt.format(i, gd, pso)) for i, (gd, pso) in enumerate(zip(GD_plot[2], PSO_plot[2]))]
+fmt = '{:<12}{:<25}{:<35}{}'
+print(fmt.format("Iteration", "Gradient descent ", "Particle Swarm Optimisation", "Evolutionary Algorithm"))
+[print(fmt.format(i, gd, pso, ea)) for i, (gd, pso, ea) in enumerate(zip(GD_plot[2], PSO_plot[2], EA_plot[2]))]
