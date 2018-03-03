b30d74055513361c0cfdcca2a1e2bc231dffcdd5
Jordy <jordyvanappeven@gmail.com>
2018-03-03 14:30:40 +0100

    Fix collision, color sensor direction

--- Tutorial1/simulation.py
+++ Tutorial1/simulation.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import collections  as mc
+from matplotlib.colors import ListedColormap, BoundaryNorm
+
 
 #Check if point c lies between line (a,b)
 def collision(walls,pos):
@@ -9,7 +11,7 @@ def collision(walls,pos):
 		ab = np.sqrt((wall[0,0] - wall[1,0])**2 + (wall[0,1] - wall[1,1])**2)
 		
 		ac = np.sqrt((wall[0,0] - pos[0]-robot_rad)**2 + (wall[0,1] - pos[1]-robot_rad)**2)
-		cb = np.sqrt((wall[1,0] - pos[0]-robot_rad)**2 + (wall[1,1] -pos[1]-robot_rad)**2)
+		cb = np.sqrt((wall[1,0] - pos[0]-robot_rad)**2 + (wall[1,1] - pos[1]-robot_rad)**2)
 		if (ac+cb <= ab+eps) & (ac+cb >= ab-eps):
 			return True
 		
@@ -17,7 +19,14 @@ def collision(walls,pos):
 		cb2 = np.sqrt((wall[1,0] - pos[0]+robot_rad)**2 + (wall[1,1] - pos[1]+robot_rad)**2)
 		if (ac2+cb2 <= ab+eps) & (ac2+cb2 >= ab-eps):
 			return True
-		
+		ac3 = np.sqrt((wall[0,0] - pos[0]+robot_rad)**2 + (wall[0,1] - pos[1]-robot_rad)**2)
+		cb3 = np.sqrt((wall[1,0] - pos[0]+robot_rad)**2 + (wall[1,1] - pos[1]-robot_rad)**2)
+		if (ac3+cb3 <= ab+eps) & (ac3+cb3 >= ab-eps):
+			return True
+		ac4 = np.sqrt((wall[0,0] - pos[0]-robot_rad)**2 + (wall[0,1] - pos[1]+robot_rad)**2)
+		cb4 = np.sqrt((wall[1,0] - pos[0]-robot_rad)**2 + (wall[1,1] - pos[1]+robot_rad)**2)
+		if (ac4+cb4 <= ab+eps) & (ac4+cb4 >= ab-eps):
+			return True
 
 def movement(Vl,Vr,pos,theta):
 		dT = 0.1
@@ -75,16 +84,16 @@ def wall_distance(sensors,walls):
 				pass
 			else: 
 				distance[i] = np.sqrt((x1-Px)**2+(y1-Py)**2)
-				i+=1
+				# i+=1
 				break
 		i +=1	
 	return distance	
 
 #Determine range of square environment polygon
 env_range = 10
-pos_start = [2,2]
+pos_start = [2,8]
 robot_rad = 0.25
-sens_range = 1
+sens_range = 2
 #add walls to 4x2x2d array, giving start- & end-coordinates 
 #for each wall surrounding the environment
 walls = np.array([[[0,0],[0,env_range]]])
@@ -103,15 +112,21 @@ plt.xlim(-12,12)
 plt.ylim(-12,12)
 lc_walls = mc.LineCollection(walls)
 
-for _ in range(1000):
+np.random.seed(5)
+for _ in range(2000):
 	Vl = np.random.randint(0,11)/100
-	Vr = np.random.randint(0,11)/100
+	# Vr = np.random.randint(0,11)/100
+	
+	Vr = Vl
+	
 	pos, theta = movement(Vl,Vr,pos,theta)
-	if collision(walls,pos):
+	while collision(walls,pos):
+	# if collision(walls,pos):
 		theta += 1/2*np.pi
-	# while collision(walls,pos):
+	
 		print('collision')
 		pos, theta = movement(Vl,Vr,pos,theta)
+		
 	sensors = init_sensors(pos,theta)
 	sens_distance = wall_distance(sensors,walls)
 	ax.clear()
@@ -119,7 +134,9 @@ for _ in range(1000):
 	_ = plt.ylim(-2,12)
 	robot = plt.Circle(pos,robot_rad)
 	# ax.scatter(pos[0],pos[1],s=(robot_rad*40)**2)
-	lc_sensors = mc.LineCollection(sensors)
+	linecolors = ['red' if i==0 else 'blue' for i in range(12)]
+	lc_sensors = mc.LineCollection(sensors,colors = linecolors)
+
 	_ = ax.add_artist(robot)
 	_ = ax.add_collection(lc_walls)
 	_ = ax.add_collection(lc_sensors)
