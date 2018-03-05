bf3054b3d4e82342f694ecd111ad31b789a47729
Jordy <jordyvanappeven@gmail.com>
2018-03-05 21:02:46 +0100

    Fix ann weight innitialisation for EA

--- Tutorial1/simulation.py
+++ Tutorial1/simulation.py
@@ -7,7 +7,7 @@ from time import time
 
 # Check if point c lies between line (a,b)
 def collision(walls, pos):
-    eps = 0.02
+    eps = 0.01
     for wall in walls:
         ab = np.sqrt((wall[0, 0] - wall[1, 0]) ** 2 + (wall[0, 1] - wall[1, 1]) ** 2)
 
@@ -16,8 +16,8 @@ def collision(walls, pos):
         if (ac + cb <= ab + eps) & (ac + cb >= ab - eps):
             return True
 
-        ac2 = np.sqrt((wall[0, 0] - pos[0] + robot_rad) ** 2 + (wall[0, 1] - pos[1] + robot_rad) ** 2)
-        cb2 = np.sqrt((wall[1, 0] - pos[0] + robot_rad) ** 2 + (wall[1, 1] - pos[1] + robot_rad) ** 2)
+        ac2 = np.sqrt((wall[0, 0] - pos[0] - robot_rad) ** 2 + (wall[0, 1] - pos[1] + robot_rad) ** 2)
+        cb2 = np.sqrt((wall[1, 0] - pos[0] - robot_rad) ** 2 + (wall[1, 1] - pos[1] + robot_rad) ** 2)
         if (ac2 + cb2 <= ab + eps) & (ac2 + cb2 >= ab - eps):
             return True
         ac3 = np.sqrt((wall[0, 0] - pos[0] + robot_rad) ** 2 + (wall[0, 1] - pos[1] - robot_rad) ** 2)
@@ -93,7 +93,7 @@ def movement(Vl, Vr, pos, theta):
     return pos_new, theta
 
 
-def ann(v_left, v_right, sensor_output):
+def ann(weights, v_left, v_right, sensor_output):
     """
     Neural network combining previous velocities and sensor distance outputs.
     (Trained) weights are multiplied with this combined vector.
@@ -105,32 +105,33 @@ def ann(v_left, v_right, sensor_output):
     """
     # append v_left and v_right to sensor_output and set correct shape
     input_vector = np.append(sensor_output, [v_left, v_right, 1])
+    # print(input_vector)
     # multiply input_input vector by weights and put through tanh activation function
     output = np.tanh(np.dot(weights, input_vector))
-
     # return vector of 2x1; v_left = output[0][0] v_right = output[1][0]
     return output
 
-
-def simulation(env_range, pos, robot_rad, sens_range, dT, weights, graphics=False):
+def simulation(iter,env_range, pos, robot_rad, sens_range, dT, weights, graphics=False):
     # Initialise velocities for right and left wheel of robot
-    Vl = np.random.randint(0, 11) / 100
-    Vr = np.random.randint(0, 11) / 100
-    # Vr = Vl
-
+    Vl = 0
+    Vr = 0
     # Set theta to viewing direction of robot
     theta = np.arctan(pos[1] / pos[0])
-
     # add walls to 4x2x2d array, giving start- & end-coordinates
     # for each wall surrounding the environment
     walls = np.array([[[0, 0], [0, env_range]]])
     walls = np.vstack((walls, np.array([[[0, 0], [env_range, 0]]])))
     walls = np.vstack((walls, np.array([[[env_range, 0], [env_range, env_range]]])))
     walls = np.vstack((walls, np.array([[[0, env_range], [env_range, env_range]]])))
+    walls = np.vstack((walls, np.array([[[3,3], [7,3]]])))
+    walls = np.vstack((walls, np.array([[[3,3], [3, 7]]])))
+    walls = np.vstack((walls, np.array([[[3,7], [7,7]]])))
+    walls = np.vstack((walls, np.array([[[7,7], [7,3]]])))
 
     # Initialise variables to measure fitness
     num_collisions = 0
-    surface_covered = {(pos[0], pos[1])}
+    x, y = np.asscalar(pos[0]), np.asscalar(pos[1])
+    surface_covered= {(np.round(x), np.round(y))}
 
     # Run simulation
     if graphics is True:
@@ -140,28 +141,19 @@ def simulation(env_range, pos, robot_rad, sens_range, dT, weights, graphics=Fals
         plt.ylim(-12, 12)
         lc_walls = mc.LineCollection(walls)
 
-    for i in range(500):
-        # Vl, Vr = 0.05,0.05
-        # print(Vl,Vr)
+    for i in range(iter):
         # Calculate new position and viewing angle according to velocities
         pos_old, theta_old = pos, theta
         pos, theta = movement(Vl, Vr, pos, theta)
         # Add unique positions to surface_covered
         x, y = np.asscalar(pos[0]), np.asscalar(pos[1])
         surface_covered.add((np.round(x), np.round(y)))
-        # When collision neglect movement orthogonal to wall only allow parralel movement
+        # When collision ignore movement orthogonal to wall only allow parralel movement
         # while collision(walls,pos):
         if collision(walls, pos):
-            # print('collision')
-            num_collisions += 1
-            # theta += 1/6*np.pi
-
-            # pos, theta = movement(Vl,Vr,pos,theta)
-            # Add unique positions to surface_covered
-            pos, theta = pos_old, theta_old
-            x, y = np.asscalar(pos[0]), np.asscalar(pos[1])
-            surface_covered.add((np.round(x), np.round(y)))
-            # Define 12 sensors each separated by 30 deg,2pi/12rad and calculate distance to any object
+        	num_collisions += 1
+        	pos, theta = pos_old, theta_old
+        # Define 12 sensors each separated by 30 deg,2pi/12rad and calculate distance to any object
         sensors = init_sensors(pos, theta)
         sens_distance = wall_distance(sensors, walls)
         if graphics is True:
@@ -175,29 +167,26 @@ def simulation(env_range, pos, robot_rad, sens_range, dT, weights, graphics=Fals
             _ = ax.add_collection(lc_walls)
             _ = ax.add_collection(lc_sensors)
             plt.pause(1e-40)
-
             # When 1/dT=1 run controller and calculate new velocities according to old vel. and sensor output
         if (i * dT) % 1 == 0:
-            # Vl, Vr = 0.5,0.5
-            Vl, Vr = ann(Vl, Vr, sens_distance)
-    fitness = len(surface_covered) / (num_collisions + 1)
-    return fitness  # ,num_collisions,len(surface_covered)
-
+            Vl, Vr = ann(weights,Vl, Vr, sens_distance)
+    fitness = len(surface_covered) / (np.log(num_collisions) + 1)
+    # plt.close()
+    return fitness
 
 def evolutionaryAlgorithm(num_iter, population_size, ndim, rn_range, benchmark_function, offspring):
     # Initialise
     population = np.random.rand(population_size, ndim[0], ndim[1]) * rn_range[0] + rn_range[1]
     average_genotype = np.mean(population[0:], axis=0)
-    fitness = benchmark_function(env_range, pos, robot_rad, sens_range, dT, weights=average_genotype, graphics=False)
+    fitness = benchmark_function(iter_sim,env_range, pos, robot_rad, sens_range, dT, weights=average_genotype, graphics=False)
     output = np.array([fitness])
     # arrays for keeping the values for the average and best fitness per iteration
     averages = np.zeros(num_iter)
     bests = np.zeros(num_iter)
-    # fig, ax2 = plt.subplots()
     fitnesess = np.zeros((num_iter, population_size))
-    plt.ion()
-    # ax = plt.subplot(111)
 
+    plt.figure(2)
+    plt.ion()
 
     for _ in range(num_iter):
         print(_)
@@ -205,8 +194,7 @@ def evolutionaryAlgorithm(num_iter, population_size, ndim, rn_range, benchmark_f
         fitness_all = []
         for i in range(population_size):
             sim_start = time()
-            fitness = benchmark_function(env_range, pos, robot_rad, sens_range, dT, weights=population[i],
-                                         graphics=False)
+            fitness = benchmark_function(iter_sim,env_range, pos, robot_rad, sens_range, dT, weights=population[i], graphics=False)
             print("simulation {} took {}".format(i, time() - sim_start))
             fitness_all = np.append(fitness_all, fitness)
         print('Fitness :', fitness_all)
@@ -220,20 +208,19 @@ def evolutionaryAlgorithm(num_iter, population_size, ndim, rn_range, benchmark_f
         for i in range(population_size):
             population[i] = np.reshape(
                 [np.mean([population[i][j][k], population[np.random.randint(population.shape[0])][j][k]]) for j in
-                 range(population.shape[1]) for k in range(population.shape[2])], (2, 14))
+                 range(population.shape[1]) for k in range(population.shape[2])], (2, 15))
         # Output
         average_genotype = np.mean(population[0:], axis=0)
         print('Average :', average_genotype)
-        fitness = benchmark_function(env_range, pos, robot_rad, sens_range, dT, weights=average_genotype,
-                                     graphics=False)
-        fitnesess[_] = fitness_all
+        fitness = benchmark_function(iter_sim,env_range, pos, robot_rad, sens_range, dT, weights=average_genotype,graphics=False)
+        
         averages[_] = fitness_all.mean()
         bests[_] = fitness_all.max()
-        # plt.clear()
-        plt.plot(range(_), averages[0:_], color="red")
-        plt.plot(bests[0:_], color="green")
-        # ax2.boxplot(fitness_all)
-        plt.pause(0.0001)
+        
+        
+        plt.plot(bests[0:_+1], color="green")
+        plt.plot(averages[0:_+1], color="red")
+        plt.pause(3)
         output = np.append(output, fitness)
     return output
 
@@ -243,34 +230,36 @@ Parameters to setup simulation for cleaning robot
 '''
 # Define range and starting point within square polygon environment
 env_range = 10
-pos = [2, 8]
+# pos = np.random.rand(2,1)*6-3
+pos = np.array([2,2])
 # Defin robot radius, sensor range, 1/dT for how many times to render simulation within one loop of robot controller
-robot_rad = 0.25
-sens_range = 0.5
-dT = 0.4
+robot_rad = 0.5
+sens_range = 1
+dT = 0.1
 np.random.seed(5)
-# Initialise weights for ann
-weights = np.random.rand(2, 15)
+iter_sim = 500
 '''
 Parameters to setup evolutionary algorithm
 '''
-num_iter = 100
-population_size = 20
-ndim = 2, 14
-rn_range = [20, -10]
-offspring = 0.2
+iter_ea = 100
+population_size = 50
+ndim = 2, 15
+rn_range = [10, -5]
+offspring = 0.5
 # fitness,num_collisions,surface_covered = simulation(env_range,pos,robot_rad,sens_range,dT,weights, graphics=False)
-avg = np.array([[3.14007371, -2.28499743, -1.50735333, -3.10024796, -2.03598207, 1.34219161
-                    , -0.96853389, 1.13621073, 2.06163012, 1.5961033, 0.20263131, -1.55570184
-                    , -1.64663837, 0.76583525],
-                [-3.21968142, -0.38475494, 1.02471362, -0.60703129, 2.29882853, -0.66518924
-                    , 1.98278355, 2.90782075, -0.71752998, -2.40418059, 1.24933194, 0.61902986
-                    , -1.12155129, -1.67256361]])
+avg = np.array(
+	[[ 0.15904197,  0.60632302, -0.27319458, -0.01744001, -0.46646285, -0.02565339,
+   0.14300986,  0.19738202 ,-0.54087545,  0.5843433,  -0.1273625,  -0.24264141,
+  -1.92671453,  0.76185036 , 0.56204834],
+ [ 0.05057849,  0.07818218 ,-0.43762829 , 0.01238846  ,0.36921576 , 0.55867089,
+  -0.34058266 ,-1.75835065 ,-1.15323556, -1.0287542 ,  0.05006896 ,-0.10920556
+,   0.43480835, -0.50853751 , 0.11581586]]
+)
 
-simulation(env_range, pos, robot_rad, sens_range, dT, weights=avg)
-output = evolutionaryAlgorithm(num_iter, population_size, ndim, rn_range, simulation, offspring)
+simulation(iter_sim,env_range, pos, robot_rad, sens_range, dT, weights=avg, graphics = True)
+
+output = evolutionaryAlgorithm(iter_ea, population_size, ndim, rn_range, simulation, offspring)
 print(output)
 
 fmt = '{:<15}{:<25}{:<25}{}'
 print(fmt.format("Iteration", "Collisions", "Surface", "Fitness"))
-# print(fmt.format(1,num_collisions,surface_covered,fitness))
