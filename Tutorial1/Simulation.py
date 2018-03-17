5575750e00535c65fe758389a028609f78a2bbed
Jordy <jordyvanappeven@gmail.com>
2018-03-17 14:07:47 +0100

    Controller map1

--- Tutorial1/Simulation.py
+++ Tutorial1/Simulation.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import collections as mc
+import localization
 
 # (Jordy van Appeven)
 class Simulation:
-    def __init__(self, iter_sim, env_range, pos, robot_rad, sens_range, dT, fitness, graphics=False, m=2):
+    def __init__(self, iter_sim, env_range, pos, robot_rad, sens_range, dT, fitness, sim_map, graphics=False):
         self.graphics = graphics
         self.sens_range = sens_range
         self.robot_rad = robot_rad
@@ -13,108 +14,110 @@ class Simulation:
         self.env_range = env_range
         self.iter_sim = iter_sim
         self.fitness = fitness
-        self.m = m
+        self.map = sim_map
 
     def simulate(self, weights):
         # Initialise velocities for right and left wheel of robot
         Vl = 0
         Vr = 0
+        # locations = np.array([[-6,-6],[6,-6],[6,6],[-6,6],[0,0]])
         pos = self.pos
+        # pos = locations[np.random.randint(locations.shape[0])]
+        # pos = np.array([0,0])
         env_range = self.env_range
         graphics = self.graphics
-        # Set theta to viewing direction of robot
-        theta = 0
-
         # add walls to 4x2x2d array, giving start- & end-coordinates
         # for each wall surrounding the environment
-        # Map1
-        if self.m == 1:
-            walls = np.array([[[-env_range / 2, -env_range / 2], [-env_range / 2, env_range / 2]]])
-            walls = np.vstack((walls, np.array([[[-env_range / 2, -env_range / 2], [env_range / 2, -env_range / 2]]])))
-            walls = np.vstack((walls, np.array([[[env_range / 2, -env_range / 2], [env_range / 2, env_range / 2]]])))
-            walls = np.vstack((walls, np.array([[[-env_range / 2, env_range / 2], [env_range / 2, env_range / 2]]])))
-            walls = np.vstack((walls, np.array([[[-env_range / 6, -env_range / 6], [env_range / 6, -env_range / 6]]])))
-            walls = np.vstack((walls, np.array([[[-env_range / 6, -env_range / 6], [-env_range / 6, env_range / 6]]])))
-            walls = np.vstack((walls, np.array([[[-env_range / 6, env_range / 6], [env_range / 6, env_range / 6]]])))
-            walls = np.vstack((walls, np.array([[[env_range / 6, env_range / 6], [env_range / 6, -env_range / 6]]])))
-        elif self.m == 2:
-            # Map2
-            walls = np.array([[[-env_range / 2, -env_range / 3], [-env_range / 2, env_range / 3]]])
-            walls = np.vstack((walls, np.array([[[-env_range / 2, env_range / 3], [-env_range / 3, env_range / 2]]])))
-            walls = np.vstack((walls, np.array([[[-env_range / 3, env_range / 2], [env_range / 3, env_range / 2]]])))
-            walls = np.vstack((walls, np.array([[[env_range / 3, env_range / 2], [env_range / 2, env_range / 3]]])))
-            walls = np.vstack((walls, np.array([[[env_range / 2, env_range / 3], [env_range / 2, -env_range / 3]]])))
-            walls = np.vstack((walls, np.array([[[env_range / 2, -env_range / 3], [env_range / 3, -env_range / 2]]])))
-            walls = np.vstack((walls, np.array([[[env_range / 3, -env_range / 2], [-env_range / 3, -env_range / 2]]])))
-            walls = np.vstack((walls, np.array([[[-env_range / 3, -env_range / 2], [-env_range / 2, -env_range / 3]]])))
-            walls = np.vstack((walls, np.array([[[-env_range / 4, -env_range / 4], [-env_range / 4, env_range / 4]]])))
-            walls = np.vstack((walls, np.array([[[-env_range / 4, env_range / 4], [-env_range / 9, env_range / 4]]])))
-            walls = np.vstack((walls, np.array([[[-env_range / 9, env_range / 4], [-env_range / 9, -env_range / 4]]])))
-            walls = np.vstack((walls, np.array([[[-env_range / 9, -env_range / 4], [-env_range / 4, -env_range / 4]]])))
-            walls = np.vstack((walls, np.array([[[env_range / 4, -env_range / 4], [env_range / 4, env_range / 4]]])))
-            walls = np.vstack((walls, np.array([[[env_range / 4, env_range / 4], [env_range / 9, env_range / 4]]])))
-            walls = np.vstack((walls, np.array([[[env_range / 9, env_range / 4], [env_range / 9, -env_range / 4]]])))
-            walls = np.vstack((walls, np.array([[[env_range / 9, -env_range / 4], [env_range / 4, -env_range / 4]]])))
+        walls = self.map
+        # Set theta to viewing direction of robot
+        theta = 0
 
         # Initialise variables to measure fitness
         num_collisions = 0
         x, y = np.asscalar(pos[0]), np.asscalar(pos[1])
         surface_covered = {(np.round(x), np.round(y))}
-
+        fitness = 0
         # Initialize instance of fitness class:
         # self.fitness = self.fitness(x, y, num_collisions)
-
+        #Initialize alpha's for kalmanFilter
+        alphas = [0.1,0.1,0.1,0.1]
+        localization_iter = 5
+        kalman = localization.kalmanFilter(self.map,pos,theta,alphas)
         # Run simulation
         if graphics is True:
-            plt.ion()
+            plt.figure(1)
             ax = plt.subplot(111)
-            plt.xlim(-env_range - 2, env_range + 2)
-            plt.ylim(-env_range - 2, env_range + 2)
+            plt.ion()
             lc_walls = mc.LineCollection(walls)
 
         for i in range(self.iter_sim):
             # Calculate new position and viewing angle according to velocities
+            
             pos_old, theta_old = pos, theta
             pos, theta = self.movement(Vl, Vr, pos, theta)
-
-            # When collision ignore movement orthogonal to wall only allow parallel movement
-            if self.collision(walls, pos):
-                num_collisions += 1
-                pos, theta = pos_old, theta_old
 
             # Add unique positions to surface_covered
             x, y = np.asscalar(pos[0]), np.asscalar(pos[1])
             surface_covered.add((np.round(x), np.round(y)))
-
-            # Define 12 sensors each separated by 30 deg,2pi/12rad and calculate distance to any object
+            
+            if self.collision(walls, pos):
+                num_collisions += 1
+                pos, theta = pos_old, theta_old
+                Vl,Vr = 0,0
             sensors = self.init_sensors(pos, theta)
             sens_distance = self.wall_distance(sensors, walls)
-
+            # Define 12 sensors each separated by 30 deg,2pi/12rad and calculate distance to any object
+            
+            #Update kalmanFilter
+            kalman.updateKalmanFilter(pos,theta)
+            
             if graphics is True:
-                view = [[pos[0], pos[0] + np.cos(theta) * self.robot_rad],
-                        [pos[1], pos[1] + np.sin(theta)] * self.robot_rad]
+                view = [[pos[0], pos[0] + np.cos(-theta) * self.robot_rad],
+                        [pos[1], pos[1] + np.sin(-theta)] * self.robot_rad]
                 ax.clear()
-                _ = plt.xlim(-env_range / 2 - 2, env_range / 2 + 2)
-                _ = plt.ylim(-env_range / 2 - 2, env_range / 2 + 2)
-                robot = plt.Circle(pos, self.robot_rad)
+                
+                
+                for j in range(localization_iter):
+                    robot = plt.Circle(kalman.sampleKalmanFilter(),self.robot_rad)
+                    _ = ax.add_artist(robot)    
                 lc_sensors = mc.LineCollection(sensors, linestyle='dotted')
+                robot = plt.Circle(pos, self.robot_rad)
                 _ = ax.add_artist(robot)
                 _ = ax.add_collection(lc_walls)
+                
                 _ = ax.add_collection(lc_sensors)
                 _ = ax.plot(view[0], view[1], c='r')
+                # xlim,ylim = ax.get_xlim(),ax.get_ylim()
+                # _ = plt.xlim(xlim)
+                # _ = plt.ylim(ylim)
                 plt.pause(1e-40)
-                # When 1/dT=1 run controller and calculate new velocities according to old vel. and sensor output
-
+                
             # Update fitness
             # self.fitness.update(x, y, num_collisions)
             # Calculate new velocities
-            Vl, Vr = self.ann(weights, Vl, Vr, sens_distance)
-
+            
+            if i*self.dT%1 == 0:
+                Vl_old,Vr_old = Vl,Vr
+                Vl, Vr = self.ann(weights, Vl, Vr, sens_distance)
+            
+            if max(sens_distance)-self.sens_range*2/3> 0: 
+                col_dist = max(sens_distance)-self.sens_range*2/3
+                if col_dist>1:
+                    col_dist = 1
+            else: 
+                col_dist = 0
+            fitness += (abs(Vl)+abs(Vr)) * (1-np.sqrt(abs(Vl-Vr)))*(1-col_dist)
+            # print((1-col_dist))
+            # print((abs(Vl)+abs(Vr)))
+            # print((1-np.sqrt(abs(Vl-Vr)))**2)
+            # print((1-np.sqrt(np.linalg.norm(Vl-Vr))))
+            # print(fitness/i)
+            # print(i)
         # Calculate final fitness:
         # fitness = self.fitness.calculate()
-        fitness = len(surface_covered) / (np.log(num_collisions + 1) + 1)
-        # print('Fitness :',fitness)
-        return fitness
+        # fitness = len(surface_covered) / (np.log(num_collisions + 1) + 1)
+        # print('Fitness :',fitness/self.iter_sim)
+        return len(surface_covered)*fitness/self.iter_sim
 
     def movement(self, Vl, Vr, pos, theta):
 
@@ -144,7 +147,7 @@ class Simulation:
             # Compute distance from position to linesegment (wall)
             x0, y0 = pos[0], pos[1]
             x1, y1, x2, y2 = wall[0, 0], wall[0, 1], wall[1, 0], wall[1, 1]
-
+            
             px, py = x2 - x1, y2 - y1
             u = ((x0 - x1) * px + (y0 - y1) * py) / (px * px + py * py)
             if u > 1:
@@ -154,13 +157,15 @@ class Simulation:
             x, y = x1 + u * px, y1 + u * py
             dx, dy = x - x0, y - y0
             distance = np.sqrt(dx * dx + dy * dy)
+            # print(distance)
             if distance < robot_rad:
                 return True
 
     # Initialise positions for 12 sensors
     def init_sensors(self, pos, theta):
-        robot_rad = self.robot_rad
-        sens_range = self.sens_range
+        eps = 0.0001
+        robot_rad = self.robot_rad-eps
+        sens_range = self.sens_range#+1*eps
         sensors = np.zeros([12, 2, 2])
         for i in range(len(sensors)):
             sensors[i] = [[pos[0] + np.sin(theta) * robot_rad,
@@ -174,7 +179,6 @@ class Simulation:
         distance = np.zeros(12)
         sens_range = self.sens_range
         i = 0
-        sens_range = self.sens_range
         for sensor in sensors:
 
             for wall in walls:
@@ -182,13 +186,13 @@ class Simulation:
                 x3, y3, x4, y4 = wall[0, 0], wall[0, 1], wall[1, 0], wall[1, 1]
                 # Repair vertical wall/sensor
                 if x1 == x2:
-                    x1 += 0.001
+                    x1 += 0.000001
                 if x3 == x4:
-                    x3 += 0.001
+                    x3 += 0.000001
                 if y1 == y2:
-                    y1 += 0.001
+                    y1 += 0.000001
                 if y3 == y4:
-                    y3 += 0.001
+                    y3 += 0.000001
                 # Calculate intersection point between wall and sensor line
                 Px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / (
                     (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
@@ -200,9 +204,10 @@ class Simulation:
                 elif (Px < np.minimum(x3, x4)) | (Px > np.maximum(x3, x4)):
                     pass
                 else:
-                    distance[i] = sens_range - (np.sqrt((x1 - Px) ** 2 + (y1 - Py) ** 2))
+                    distance[i] = sens_range-(np.sqrt((x1 - Px) ** 2 + (y1 - Py) ** 2)) #+0.32
                     break
             i += 1
+        # print(distance)
         return distance
 
     # (Sebas Higler)
@@ -219,15 +224,19 @@ class Simulation:
         layers = weights.shape[0]
 
         # append v_left and v_right to sensor_output and set correct shape
-        input_vector = np.append(sensor_output, [v_left, v_right, 1])
+        input_vector = np.append(sensor_output, [v_left, v_right,1])
         # Calculate 10 node hidden layer with sigmoid activation
         for l in range(layers - 1):
             input_vector = 1 / (1 + np.exp(-np.matmul(input_vector, weights[l])))
         # Calculate output nodes by hyperbolic tangent activation
-        output = np.tanh(np.dot(input_vector, weights[layers - 1]))
-
+        # output = np.tanh(np.dot(input_vector, weights[layers - 1]))
+        # print(input_vector)
         # multiply input_input vector by weights and put through tanh activation function
-        # output = 1 / (1 + np.exp(-np.dot(weights, input_vector)))
+        output = 1 / (1 + np.exp(-np.matmul(input_vector, weights[layers - 1])))
         # return vector of 2x1; v_left = output[0][0] v_right = output[1][0]
-
+        # print(output)
+        output = output-0.5
+        # print(output)
+        # output = [-0.5,-0.5]
+        # print(output)
         return output
--- Tutorial1/evolutionary_alg.py
+++ Tutorial1/evolutionary_alg.py
@@ -12,17 +12,19 @@ import selectionreproduction
 def evolutionaryAlgorithm(num_iter, population_size, layers, ndim, rn_range, benchmark_function,
                           crossover_prob, mutation_prob, selection_method, graphics=False):
     # Initialise # (Jordy van Appeven)
+    # population = np.array(
+    #     [np.array([np.random.rand(ndim[l], ndim[l + 1]) * rn_range[0] + rn_range[1] for l in range(layers)]) for _ in
+    #      range(population_size)])
     population = np.array(
-        [np.array([np.random.rand(ndim[l], ndim[l + 1]) * rn_range[0] + rn_range[1] for l in range(layers)]) for _ in
+        [np.array([np.random.normal(size=(ndim[l], ndim[l + 1]),loc=0, scale=rn_range[0]) for l in range(layers)]) for _ in
          range(population_size)])
-    average_genotype = np.mean(population[:], axis=0)
-    fitness = benchmark_function(average_genotype)
-    output = np.array([fitness])
+    np.random.normal(loc=0, scale=rn_range[0])
     # arrays for keeping the values for the average and best fitness per iteration
     averages = np.zeros(num_iter)
     bests = np.zeros(num_iter)
     fitnesess = np.zeros((num_iter, population_size))
     diversities = np.zeros(num_iter)
+    
     pool = Pool(processes=4)
 
     if graphics:
@@ -34,65 +36,73 @@ def evolutionaryAlgorithm(num_iter, population_size, layers, ndim, rn_range, ben
         plt.ion()
 
     for _ in range(num_iter):
+        locations = np.array([[-6,-6],[6,-6],[6,6],[-6,6],[0,0]])
+        pos = locations[np.random.randint(locations.shape[0])]
+        
+        benchmark_function.pos = pos
         print(_)
         # Evaluation
         fitness_all = []
         sim_start = time()
-        fitness_all = np.array(pool.map(benchmark_function, population))
+        fitness_all = np.array(pool.map(benchmark_function.simulate, population))
         print("time for experiments: {}".format(time() - sim_start))
         print('Fitness :', fitness_all)
-        print("population: {}".format(population))
 
         diversity = 0
         for i in range(population_size):
             for l in range(layers):
-                for j in range(i, population_size):
-                    diversity += np.linalg.norm(population[i][l].flatten() - population[j][l].flatten())
+                for w in range(ndim[l]):
+                    for j in range(i,population_size):
+                        diversity += np.linalg.norm(population[i][l][w]-population[j][l][w])
         print("diversity: {}".format(diversity))
-
-        # Selection & Reproduction # (Sebas Higler)
-        population = selection_method(fitness_all, population)
-
-        best_genotype = population[0]
+        fitnesess[_] = fitness_all
+        best_genotype = population[np.argmax(fitness_all)]
         print('Best :', best_genotype)
-
-        # Crossover/Mutation
-        reshape = [0 for x in range(layers)]
-        for i in range(population_size):
-            if np.random.rand() < crossover_prob:
-                for l in range(layers):
-                    reshape[l] = [ndim[l], ndim[l + 1]]
-                    population[i][l] = np.reshape(np.array(
-                        [np.mean([population[i][l][j][k], population[np.random.randint(population_size)][l][j][k]]) for
-                         j in range(ndim[l]) for k in range(ndim[l + 1])]), reshape[l])
-                    # population[i][l] = population[i][l].reshape(reshape[l])    
-
-        for i in range(population_size):
-            if np.random.rand() < mutation_prob:
-                layer_index = np.random.randint(layers)
-                print(layer_index)
-                weight_index = np.random.randint(ndim[layer_index]), np.random.randint(ndim[layer_index + 1])
-                mutation = np.random.normal(loc=0, scale=rn_range[0] / 10)
-                print("mutating {} at [{}][{}] with {}".format(i, layer_index, weight_index, mutation))
-                population[i][layer_index][weight_index] += mutation
-
-        # Output
-        average_genotype = np.mean(population[:], axis=0)
-        print('Average :', average_genotype)
-        fitness = benchmark_function(average_genotype)
-
         averages[_] = fitness_all.mean()
         bests[_] = fitness_all.max()
         diversities[_] = diversity
+        # Selection & Reproduction # (Sebas Higler)
+        population = selection_method(fitness_all, population)
+        
+        # np.save('pop',population)
+        # Crossover/Mutation
+        cross = 0
+        for i in range(population_size):
+            for l in range(layers):
+                for w in range(ndim[l]*ndim[l+1]):
+                    if np.random.rand() < crossover_prob:
+                        cross+=1
+                        weight_index = np.random.randint(ndim[l]), np.random.randint(ndim[l + 1])
+                        weight_temp = population[i][l][weight_index]
+                        crossover_partner = np.random.randint(population_size)
+                        population[i][l][weight_index] = population[crossover_partner][l][weight_index]
+                        population[crossover_partner][l][weight_index] = weight_temp
+        mut = 0                
+        for i in range(population_size):
+            for l in range(layers):
+                for w in range(ndim[l]*ndim[l+1]):
+                    if np.random.rand() < mutation_prob:
+                        mut+=1
+                        # layer_index = np.random.randint(layers)
+                        # print(layer_index)
+                        weight_index = np.random.randint(ndim[l]), np.random.randint(ndim[l + 1])
+                        mutation = np.random.normal(loc=0, scale=rn_range[0])
+                        # print("mutating {} at [{}][{}] with {}".format(i, l, weight_index, mutation))
+                        population[i][l][weight_index] += mutation
+        print('Crossover :',cross)
+        print('Mutation : ',mut)
+        
+        
 
         # (Jordy van Appeven)
         if graphics:
             fitness_plt.plot(bests[0:_ + 1], color="green")
             fitness_plt.plot(averages[0:_ + 1], color="red")
-            div_plt.plot(diversities[0:_ + 1], color="orange")
+            div_plt.plot((diversities[0:_ + 1]), color="orange")
+            div_plt.set_yscale('log')
             plt.pause(3)
-        output = np.append(output, fitness)
-    return output, population[fitness_all.argmax()], diversities
+        
+    return fitnesess, best_genotype, diversities
 
 
 # if-statement necessary for multiprocessing on Windows:
@@ -102,35 +112,35 @@ if __name__ == "__main__":
     '''
     # Define range and starting point within square polygon environment
     env_range = 20
-    # pos = np.random.rand(2,1)*6-3
     pos = np.array([-6, -6])
     # Defin robot radius, sensor range, 1/dT for how many times to render simulation within one loop of robot controller
     robot_rad = 1
     sens_range = 3
-    dT = 0.1
-    np.random.seed(5)
-    iter_sim = 1000
+    dT = 1/3
+    # np.random.seed(5)
+    iter_sim = 500
     '''
     Parameters to setup evolutionary algorithm
     '''
-    iter_ea = 100
+    iter_ea = 300
     population_size = 100
     # Choose number of layers besides input layer, so hidden+output is 2 layers
-    layers = 2
-    # Input layer 15 nodes(12sensors/2velocities/1bias), arbitrarily number of hidden nodes, 2 output nodes (velocities)
-    ndim = [15, 10, 2]
-    rn_range = [10, -5]
+    layers = 1
+    # Input layer 15 nodes(12 sensors/2 velocities/1 bias), arbitrarily number of hidden nodes, 2 output nodes (velocities)
+    ndim = [15,2]
+    rn_range = [1, -1]
     # Selection criteria
-    offspring = 0.5
-    crossover = 0.2
-    mutation = 0.3
+    offspring = 0.2
+    crossover = 0.1
+    mutation = 0.05
     # Selection choose between TruncatedRankBased(offspring)/Tournament(k)
-    selection = selectionreproduction.Tournament(10).apply
+    selection = selectionreproduction.Tournament(8).apply
 
-    sim = Simulation(iter_sim, env_range, pos, robot_rad, sens_range, dT, fitness.OurFirstFitnessFunction)
+    sim_map = np.load('Maps/'+'Map1'+'.npy')
+    sim = Simulation(iter_sim, env_range, pos, robot_rad, sens_range, dT, fitness.OurFirstFitnessFunction,sim_map)
 
     fitness, best_individual, diversities = evolutionaryAlgorithm(iter_ea, population_size, layers, ndim, rn_range,
-                                                                  sim.simulate, crossover, mutation,
+                                                                  sim, crossover, mutation,
                                                                   selection,
                                                                   True)
     print('Fitness :', fitness)
@@ -152,7 +162,6 @@ if __name__ == "__main__":
 
     f.close()
 
-
     # Function to import weights
     def tokenizer(fname):
         with open(fname) as f:
@@ -163,7 +172,6 @@ if __name__ == "__main__":
                     weights = []
                     continue
                 weights.append(line)
-
 
     # Import weights
     weights = np.array([np.loadtxt(A, delimiter=',') for A in tokenizer('weights.txt')])
